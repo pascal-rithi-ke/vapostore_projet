5ec6c8a2c0e752ae9a457c1aafc4761d15548abc
pascal-rithi-ke <pascal.rithi@gmail.com>
2025-01-28 00:26:24 +0100

    missing sanctum

--- backend-render/config/app.php
+++ backend-render/config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Laravel\Sanctum\SanctumServiceProvider::class,
         /*
          * Package Service Providers...
          */
