63790ace9a7edc3a0e4ffd7cb27ee9ec87cecad4
pascal-rithi-ke <pascal.rithi@gmail.com>
2025-01-28 00:55:12 +0100

    cors

--- backend-render/config/cors.php
+++ backend-render/config/cors.php
@@ -15,14 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'https://vapostore-projet.vercel.app',
-        'https://vapostore-projet.onrender.com',
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
@@ -32,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
