e0e69f9bc05e00111fe9a91c5f2312e30e3339de
pascal-rithi-ke <pascal.rithi@gmail.com>
2025-01-27 23:27:30 +0100

    just site url + deployment url

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://vapostore-projet.vercel.app/'],
+    'allowed_origins' => ['https://vapostore-projet.vercel.app', 'https://vapostore-projet-niixcwvh9-pascal-rithi-kes-projects.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
