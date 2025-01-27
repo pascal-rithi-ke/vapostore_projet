c7ec693018f24ac90f560bea334b24155ee1e7a2
pascal-rithi-ke <pascal.rithi@gmail.com>
2025-01-27 23:58:42 +0100

    try cors

--- backend-render/public/index.php
+++ backend-render/public/index.php
@@ -49,6 +49,14 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
+header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers:*");
+
+if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {//send back preflight request response
+return "";
+}
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
