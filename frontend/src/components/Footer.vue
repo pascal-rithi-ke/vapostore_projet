927d2ab5b57b9f5b84e4d5c2bdbaa9dcbcddf838
pascal-rithi-ke <pascal.rithi@gmail.com>
2025-01-27 17:44:30 +0100

    add link repo

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,6 +1,10 @@
 <template>
     <footer class="bg-black text-white p-4">
-        <p class="text-center font-bold">Kien Pascal</p>
-        <p class="text-center">© 2025 VapoStore Project</p>
+        <p class="text-center font-bold">© Kien Pascal 2025 VapoStore Project</p>
+        <p class="text-center hover:text-secondary">
+            <a href="https://github.com/pascal-rithi-ke/vapostore_projet">
+                Lien du projet : https://github.com/pascal-rithi-ke/vapostore_projet
+            </a>
+        </p>
     </footer>
 </template>
