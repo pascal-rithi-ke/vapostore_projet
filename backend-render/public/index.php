116280fab3b028f2a4e1537e5ec11ea9d44f451e
pascal-rithi-ke <pascal.rithi@gmail.com>
2025-01-28 02:40:42 +0100

    front bug

--- backend-render/public/index.php
+++ backend-render/public/index.php
@@ -51,12 +51,11 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 header("Access-Control-Allow-Origin: https://vapostore-projet.vercel.app"); // Remplace * par ton origine exacte
 header("Access-Control-Allow-Credentials: true"); // Ajoute les credentials
-//header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // Autorise les méthodes nécessaires
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // Autorise les méthodes nécessaires
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With"); // Liste spécifique des headers autorisés
 
-if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") { // Pré-requêtes CORS
-    http_response_code(200);
-    exit();
+if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {//send back preflight request response
+return "";
 }
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
