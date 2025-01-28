c6b7d142ca138c5fab19764b6a0c61892e5892c2
pascal-rithi-ke <pascal.rithi@gmail.com>
2025-01-28 01:09:15 +0100

    cors

--- backend-render/config/sanctum.php
+++ backend-render/config/sanctum.php
@@ -16,7 +16,7 @@ return [
     */
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
+        'vapostore-projet.vercel.app,%s%s',
         'localhost,localhost:5173,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort()
     ))),
